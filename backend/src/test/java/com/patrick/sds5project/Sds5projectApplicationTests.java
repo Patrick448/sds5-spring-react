36e89a1dfa031b9b138bdc683b187040f3f72d8c
Patrick448 <patrickcarvalho448@gmail.com>
2021-11-01 10:11:44 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/patrick/sds5project/Sds5projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.sds5project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds5projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
